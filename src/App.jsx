4f5988e2feb57884aa1e1235b8be39a9b833c15b
FadhilFirmansyah <fadhilfirmansyah100@gmail.com>
2024-10-07 15:55:50 +0700

    Update tag code

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Hello, Fadhil Firmansyah</h2>
+      <h2><code>Hello, Fadhil Firmansyah</code></h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
