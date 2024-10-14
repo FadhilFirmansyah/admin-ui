e5b03e63d32af875647e39ea211b24d91f282ded
FadhilFirmansyah <fadhilfirmansyah100@gmail.com>
2024-10-14 17:28:55 +0700

    router website

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
+import ErrorRoute from "./pages/errorRoute";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Halaman Utama</div>,
+      errorElement: <ErrorRoute/>
+    },
+    {
+      path: "/login",
+      element: <SignInPage/>,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage/>,
+    },
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2><code>Hello, Fadhil Firmansyah</code></h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <RouterProvider router={myRouter} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Elements/Logo/index.jsx
@@ -0,0 +1,10 @@
+function index() {
+  return (
+    <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl text-emerald-500">
+          <span className="font-bold text-emerald-600">FINE</span>bank
+          <span className="font-bold text-emerald-600">.IO</span>
+    </div>
+  )
+}
+
+export default index
